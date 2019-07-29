663f644d3ebe48b2dbc937785baad2cdeeed2506
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-07-29 09:36:19 +0200

    add conditionals

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@
 //añado todas mis variables
 const counter = document.querySelector('.counter');
 const input = document.querySelector('.input-game');
+
 const btn = document.querySelector('.btn');
 const feedback = document.querySelector('.feedback');
 
@@ -12,6 +13,20 @@ function getRandomNumber(max) {
 //ejecuto funcion de random number
 let winnerNumber = getRandomNumber(100);
 console.log(winnerNumber);
+
+function funcionquevoyaejecutar () {
+  if (userNumber === winnerNumber) {
+    feedback.innerHTML = `¡HAS GANADO, CAMPEONA!`;
+  }
+
+  else if (userNumber > winnerNumber) {
+    feedback.innerHTML = `demasiado alto`;
+  }
+
+  else (userNumber < winnerNumber) {
+    feedback.innerHTML = `demasiado bajo`;
+  }
+}
 
 //declaro evento
 btn.addEventListener('click',funcionquevoyaejecutar)
