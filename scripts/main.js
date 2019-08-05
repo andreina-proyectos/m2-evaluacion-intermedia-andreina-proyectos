34939c78d2a2066829d9ecef5e267481e0735131
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-08-05 20:54:58 +0200

    improve feedback and add more changes on function and styles changes

--- scripts/main.js
+++ scripts/main.js
@@ -19,30 +19,42 @@ console.log(winnerNumber);
 const checkUsernumber = () => {
   let userNumber = parseInt(input.value);
   if (userNumber === winnerNumber) {
-    feedback.innerHTML = `¡HAS GANADO, CAMPEONA!`;
-    counter=0;
+    feedback.innerHTML = `¡HAS GANADO, CAMPEONA!🏆💃🏼`;
+    input.value = 'FELICIDADES!🧙🍀'
+    counter = 0;
   }
 
   else if (userNumber > winnerNumber) {
-    feedback.innerHTML = `demasiado alto`;
+    feedback.innerHTML = `demasiado alto! 😎`;
     if (counter>4) {
+      feedback.innerHTML = `INTENTALO DE NUEVO!👆🏼`
       counter = 0;
+      input.value = '';
+      input.placeholder = 'otra vez!';
+      
     }
-    counter = counter+1;
+    else {counter = counter+1;}
     counter__game.innerHTML =counter;
   }
 
   else  {
-    feedback.innerHTML = `demasiado bajo`;
+    feedback.innerHTML = `demasiado bajo! 😱`;
     if (counter>4) {
+      feedback.innerHTML = `INTENTALO DE NUEVO!👆🏼`
       counter = 0;
+      input.value = '';
+      input.placeholder = 'otra vez!';
     }
-    counter = counter+1;
+    else {counter = counter+1;}
     counter__game.innerHTML =counter;
+  }
+
+  if (userNumber > 100 || userNumber < 0) {
+    feedback.innerHTML = `El numero debe ser entre 0 y 100!🖖🏼`
   }
 }
 
+
+
 //declaro y ejecuto evento
-btn.addEventListener('click', checkUsernumber)
-
-
+btn.addEventListener('click', checkUsernumber);
