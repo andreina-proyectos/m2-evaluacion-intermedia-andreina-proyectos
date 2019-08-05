56847d7d1d50d2fcd0489db592ba7dbb74ccfbeb
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-08-05 20:17:19 +0200

    fix counter, the final countdown yay

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,7 @@ const input = document.querySelector('.input-game');
 const btn = document.querySelector('.btn');
 const feedback = document.querySelector('.feedback');
 
-let counter = 0
-
+let counter = 0;
 
 //declaro funcion de random number
 function getRandomNumber(max) {
@@ -21,21 +20,26 @@ const checkUsernumber = () => {
   let userNumber = parseInt(input.value);
   if (userNumber === winnerNumber) {
     feedback.innerHTML = `¡HAS GANADO, CAMPEONA!`;
+    counter=0;
   }
 
   else if (userNumber > winnerNumber) {
     feedback.innerHTML = `demasiado alto`;
-    counter+=1;
+    if (counter>4) {
+      counter = 0;
+    }
+    counter = counter+1;
     counter__game.innerHTML =counter;
   }
 
   else  {
     feedback.innerHTML = `demasiado bajo`;
-    counter+=1;
+    if (counter>4) {
+      counter = 0;
+    }
+    counter = counter+1;
     counter__game.innerHTML =counter;
   }
-  counter = counter + 1;
-
 }
 
 //declaro y ejecuto evento
